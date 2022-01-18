067e8312e7a90baeed9e650e74457ebb52e3fefb
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2022-01-18 22:11:42 +0800

    Update login.py

--- jbot/diy/login.py
+++ jbot/diy/login.py
@@ -1,15 +1,12 @@
-import asyncio, os
-from os.path import exists
+
+import asyncio, os, json, os, re, sys, time, requests, traceback, qrcode
 from telethon import TelegramClient, events
-from .. import client, jdbot, chat_id, _JdDir, logger
+from .. import client, jdbot, chat_id, _JdDir, logger, QR_IMG_FILE
 from ..bot.utils import V4, press_event, row, split_list
-import json, os, re, sys, time, requests, traceback
 from asyncio import exceptions
 from telethon import events, Button
 
-
 userfile = "/jd/jbot/diy/user.py" if V4 else "/ql/jbot/diy/user.py"
-
 
 def restart():
     text = "if [ -d '/jd' ]; then cd /jd/jbot; pm2 start ecosystem.config.js; cd /jd; pm2 restart jbot; else " \
@@ -17,6 +14,17 @@ def restart():
            "nohup python3 -m jbot >/ql/log/bot/bot.log 2>&1 & fi "
     os.system(text)
 
+def creat_qr(text):
+    """实例化QRCode生成qr对象"""
+    qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=10, border=4)
+    qr.clear()
+    # 传入数据
+    qr.add_data(text)
+    qr.make(fit=True)
+    # 生成二维码
+    img = qr.make_image()
+    # 保存二维码
+    img.save(QR_IMG_FILE)
 
 @jdbot.on(events.NewMessage(from_users=chat_id, pattern=r'^/user$'))
 async def user_login(event):
@@ -24,25 +32,50 @@ async def user_login(event):
         if not os.path.exists(userfile):
             await jdbot.send_message(chat_id, f'user.py不存在\n请先执行以下命令\n再执行：/user\n\n```/cmd cd {_JdDir}/jbot/diy && wget https://ghproxy.com/https://raw.githubusercontent.com/Annyoo2021/mybot/main/jbot/diy/user.py```')
             return
-        login = False
+        tellogin, qrlogin = False, False
         sender = event.sender_id
         session = "/jd/config/user.session" if V4 else "/ql/config/user.session"
         async with jdbot.conversation(sender, timeout=120) as conv:
-            msg = await conv.send_message("请做出你的选择")
-            buttons = [
-                Button.inline("重新登录", data="relogin") if os.path.exists(session) else Button.inline("我要登录", data="login"),
-                Button.inline('取消会话', data='cancel')
-            ]
-            msg = await jdbot.edit_message(msg, '请做出你的选择：', buttons=split_list(buttons, row))
-            convdata = await conv.wait_event(press_event(sender))
-            res = bytes.decode(convdata.data)
-            if res == 'cancel':
-                await jdbot.edit_message(msg, '对话已取消')
-                return
-            else:
-                await jdbot.delete_messages(chat_id, msg)
-                login = True
-        if login:
+            while True:
+                msg = await conv.send_message("请做出你的选择")
+                buttons = [
+                    Button.inline("重新登录", data="relogin") if os.path.exists(session) else Button.inline("我要登录", data="login"),
+                    Button.inline('取消会话', data='cancel')
+                ]
+                opt_btns = [
+                    Button.inline('上级目录', data='upper menu'),
+                    Button.inline('取消会话', data='cancel')
+                ]
+                msg = await jdbot.edit_message(msg, '请做出你的选择：', buttons=split_list(buttons, row))
+                convdata = await conv.wait_event(press_event(sender))
+                res = bytes.decode(convdata.data)
+                if res == 'cancel':
+                    await jdbot.edit_message(msg, '对话已取消')
+                    return
+                else:
+                    btns = [
+                        Button.inline('手机登录', data='tellogin'),
+                        Button.inline('扫码登录', data='qrlogin')
+                    ]
+                    newbtns = split_list(btns, row)
+                    newbtns.append(opt_btns)
+                    msg = await jdbot.edit_message(msg, '请做出你的选择：', buttons=newbtns)
+                    convdata = await conv.wait_event(press_event(sender))
+                    res2 = bytes.decode(convdata.data)
+                    if res2 == 'cancel':
+                        await jdbot.edit_message(msg, '对话已取消')
+                        conv.cancel()
+                        return
+                    elif res2 == 'upper menu':
+                        await jdbot.delete_messages(chat_id, msg)
+                        continue
+                    elif res2 == 'tellogin':
+                        tellogin = True
+                        break
+                    else:
+                        qrlogin = True
+                        break
+        if tellogin:
             await client.connect()
             async with jdbot.conversation(sender, timeout=100) as conv:
                 msg = await conv.send_message('请输入手机号：\n例如：`+8618888888888`\n前面一定带上区号、中国为+86')
@@ -52,6 +85,15 @@ async def user_login(event):
                 code = await conv.get_response()
                 await client.sign_in(phone.raw_text, code.raw_text.replace('code', ''))
                 await jdbot.send_message(chat_id, '恭喜您已登录成功！\n自动重启中！')
+            restart()
+        elif qrlogin:
+            await client.connect()
+            qr_login = await client.qr_login()
+            creat_qr(qr_login.url)
+            await jdbot.send_message(chat_id, '请使用TG扫描二维码以开启USER', file=QR_IMG_FILE)
+            await qr_login.wait(timeout=100)
+            await jdbot.send_message(chat_id, '恭喜您已登录成功！\n自动重启中！')
+            os.remove(QR_IMG_FILE)
             restart()
     except asyncio.exceptions.TimeoutError:
         await jdbot.edit_message(msg, '登录已超时，对话已停止')
