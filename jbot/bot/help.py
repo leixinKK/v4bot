248b3e90d2d8d7ad41594b64334fd683b5032305
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2022-01-19 14:29:13 +0800

    Update help.py

--- jbot/bot/help.py
+++ jbot/bot/help.py
@@ -1,6 +1,6 @@
 from telethon import events
 from .. import jdbot, chat_id
-
+from ..bot.utils import V4, QL
 
 @jdbot.on(events.NewMessage(from_users=chat_id, pattern='^/help'))
 async def my_help(event):
@@ -10,31 +10,72 @@ async def my_help(event):
         text = msg_text[-1]
     else:
         text = 'mhelp'
-    mhelp = '''
+    if V4:
+        mhelp = '''
 a-自定义快捷按钮
-addcron-增加定时
 bean-获取收支
-clearboard-删除快捷按钮
+blockcookie-屏蔽账号
 chart-统计收支变化
-cron-管理定时设定
+checkcookie-检测过期
+clearboard-删除快捷输入按钮
 cmd-执行cmd命令
-edit-编辑文件
-env-管理环境变量
 dl-下载文件
-getfile-获取目录下文件
+edit-编辑文件
+export-管理环境变量
+help-获取帮助
+getcookie-扫码获取cookie
+getfile-获取jd目录下文件
 log-选择日志
 node-执行js脚本文件，绝对路径
+restart-重启本程序
+repo-仓库管理
 set-BOT设置
 setname-设置命令别名
 setshort-设置自定义按钮
 snode-选择脚本后台运行
-start-开始使用本程序'''
-    bean = '/bean 加数字，获取该账户近期收支情况\n/bean in\out获取所有账户近期收或支情况\n/bean 获取账户总豆数量'
-    cmd = '/cmd用于执行cmd命令，如果命令持续10分钟仍未结束，将强行终止，以保障机器人响应'
-    edit = '/edit 进入/jd目录选择文件进行编辑，仅限简易编辑\n/edit /xx/config进入config目录选择文件编辑\n/edit /xx/config/config.sh 直接编辑config.sh文件'
-    node = '/node 用于执行js脚本 用法：\n/node /jd/own/abc/def.js'
-    getfile = '/getfile 进入/jd目录选择文件进行获取\n/getfile /xx/config进入config目录选择文件获取\n/getfile /xx/config/config.sh 直接获取config.sh文件'
-    setshort = '/setshort 用于设置快捷方式，格式如下：\n更新-->jup\nAAA-->BBB这种格式使用/a选择\n/bean 1\n/edit /xx/config/config.sh\n以“/”开头的为机器人命令快捷，使用/b选择'
+start-开始使用本程序
+upbot-更新机器人
+ver-版本'''
+        bean = '/bean 加数字，获取该账户近期收支情况\n/bean in\out获取所有账户近期收或支情况\n/bean 获取账户总豆数量'
+        cmd = '/cmd用于执行cmd命令，如果命令持续10分钟仍未结束，将强行终止，以保障机器人响应'
+        edit = '/edit 进入/jd目录选择文件进行编辑，仅限简易编辑\n/edit /jd/config进入config目录选择文件编辑\n/edit /jd/config/config.sh 直接编辑config.sh文件'
+        node = '/node 用于执行js脚本 用法：\n/node /jd/own/abc/def.js'
+        getfile = '/getfile 进入/jd目录选择文件进行获取\n/getfile /jd/config进入config目录选择文件获取\n/getfile /jd/config/config.sh 直接获取config.sh文件'
+        setshort = '/setshort 用于设置快捷方式，格式如下：\n更新-->jup\nAAA-->BBB这种格式使用/a选择\n/bean 1\n/edit /jd/config/config.sh\n以“/”开头的为机器人命令快捷，使用/b选择'
+    else:
+        mhelp = '''
+a-自定义快捷按钮
+addenv-青龙新增环境变量
+bean-获取收支
+blockcookie-屏蔽账号
+chart-统计收支变化
+clearboard-删除快捷输入按钮
+cmd-执行cmd命令
+dl-下载文件
+edit-编辑文件
+env-青龙管理环境变量
+export-V4管理环境变量
+help-获取帮助
+getcookie-扫码获取cookie
+getfile-获取jd目录下文件
+log-选择日志
+list-列出已拓展功能
+node-执行js脚本文件，绝对路径
+restart-重启本程序
+repo-仓库管理
+set-BOT设置
+setname-设置命令别名
+setshort-设置自定义按钮
+snode-选择脚本后台运行
+start-开始使用本程序
+upbot-更新机器人
+ver-版本'''
+        bean = '/bean 加数字，获取该账户近期收支情况\n/bean in\out获取所有账户近期收或支情况\n/bean 获取账户总豆数量'
+        cmd = '/cmd用于执行cmd命令，如果命令持续10分钟仍未结束，将强行终止，以保障机器人响应'
+        edit = '/edit 进入/ql目录选择文件进行编辑，仅限简易编辑\n/edit /ql/config进入config目录选择文件编辑\n/edit /ql/config/config.sh 直接编辑config.sh文件'
+        node = '/node 用于执行js脚本 用法：\n/node /ql/own/abc/def.js'
+        getfile = '/getfile 进入/jd目录选择文件进行获取\n/getfile /ql/config进入config目录选择文件获取\n/getfile /ql/config/config.sh 直接获取config.sh文件'
+        setshort = '/setshort 用于设置快捷方式，格式如下：\nAAA-->BBB这种格式使用/a选择\n/bean 1\n/edit /ql/config/config.sh\n以“/”开头的为机器人命令快捷，使用/b选择'
     snode = '/snode 选择脚本并运行'
     chart = '/chart 加数字，统计该账户近期收支情况'
     botset = '''/set 
