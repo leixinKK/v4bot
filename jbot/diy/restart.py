b9d4f8025b6686dd267de463c0964dad89a83b22
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2022-01-21 21:07:21 +0800

    Update restart.py

--- jbot/diy/restart.py
+++ jbot/diy/restart.py
@@ -10,7 +10,7 @@ import os, sys
 async def myrestart(event):
     try:
         await jdbot.send_message(chat_id, "重启程序")
-        cmdtext = "if [ -d '/jd' ]; then cd /jd/jbot; pm2 start ecosystem.config.js; cd /jd; pm2 restart jbot; else " \
+        cmdtext = "if [ -d '/jd' ]; then cd /jd/jbot; pm2 start ecosystem.config.js; else " \
                   "ps -ef | grep 'python3 -m jbot' | grep -v grep | awk '{print $1}' | xargs kill -9 2>/dev/null; " \
                   "nohup python3 -m jbot >/ql/log/bot/bot.log 2>&1 & fi "
         os.system(cmdtext)
